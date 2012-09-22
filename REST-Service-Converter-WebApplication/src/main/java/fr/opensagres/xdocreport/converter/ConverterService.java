03dd25b924c43868711abf5e07b8df50d3bf77ba
pascal leclercq <pascal.leclercq@gmail.com>
2012-09-23 00:13:29 +0200

    STARTED - task 152: Develop REST Service Converter + WebApplication http://code.google.com/p/xdocreport/issues/detail?id=152

--- REST-Service-Converter-WebApplication/src/main/java/fr/opensagres/xdocreport/converter/ConverterService.java
+++ REST-Service-Converter-WebApplication/src/main/java/fr/opensagres/xdocreport/converter/ConverterService.java
@@ -1,33 +1,65 @@
 package fr.opensagres.xdocreport.converter;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import fr.opensagres.xdocreport.core.document.DocumentKind;
+
 /**
  * REST Web Service
  *
- * @author duncan
+ * @author pleclercq
  */
 @Path("generic")
 public class ConverterService {
 
 	private  int i=0;
 
-    /**
-     * Creates a new instance of GenericResource
-     */
-    public ConverterService() {
-    }
+
 
     /**
-     * Retrieves representation of an instance of nl.dabloem.jaxrs.GenericResource
+     * Say Hello...
      * @return an instance of java.lang.String
      */
     @GET
     @Produces("text/plain")
     public String getText() {
+
+
         return "Hello "+i++;
     }
 
+
+    @GET
+    @Path("convertPDF")
+    @Produces("application/pdf")
+    public byte[] convertPDF()  {
+    	ByteArrayOutputStream out = new ByteArrayOutputStream();
+    	// 1) Create options ODT 2 PDF to select well converter form the registry
+    	Options options = Options.getFrom(DocumentKind.ODT).to(ConverterTypeTo.PDF);
+
+    	// 2) Get the converter from the registry
+    	IConverter converter = ConverterRegistry.getRegistry().getConverter(options);
+
+    	// 3) Convert ODT 2 PDF
+
+		try {
+			InputStream in = new FileInputStream(new File("/Users/pascalleclercq/git/xdocreport.samples/REST-Service-Converter-WebApplication/ODTCV.odt"));
+			converter.convert(in, out, options);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (XDocConverterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	return out.toByteArray();
+    }
 }
