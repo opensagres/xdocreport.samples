3553ed196d2488631a41564903655480763e28b1
angelo.zerr@gmail.com <angelo.zerr@gmail.com>
2012-10-04 17:15:54 +0200

    Fix pb with JUNit REST converter.

--- REST-Service-Converter-WebApplication/src/test/java/fr/opensagres/xdocreport/converter/JAXWSResourcesServiceClientTestCase.java
+++ REST-Service-Converter-WebApplication/src/test/java/fr/opensagres/xdocreport/converter/JAXWSResourcesServiceClientTestCase.java
@@ -3,12 +3,18 @@ package fr.opensagres.xdocreport.converter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.attachment.ByteDataSource;
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.cxf.jaxrs.ext.multipart.InputStreamDataSource;
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -18,72 +24,110 @@ import fr.opensagres.xdocreport.converter.internal.BinaryFileMessageBodyWriter;
 import fr.opensagres.xdocreport.converter.internal.ConverterResourceImpl;
 import fr.opensagres.xdocreport.core.io.IOUtils;
 
-public class JAXWSResourcesServiceClientTestCase {
+public class JAXWSResourcesServiceClientTestCase
+{
 
-	private static final int PORT = 8080;
+    private static final int PORT = 8082;
 
-	private static final String ROOT_ADDRESS = "http://localhost:" + PORT;
+    private static final String ROOT_ADDRESS = "http://localhost:" + PORT;
 
-	private static final String BASE_ADDRESS = ROOT_ADDRESS + "/generic";
+    private static final String BASE_ADDRESS = ROOT_ADDRESS;
 
-	private String root = JAXWSResourcesServiceClientTestCase.class
-			.getClassLoader().getResource(".").getFile();
+    private String root = JAXWSResourcesServiceClientTestCase.class.getClassLoader().getResource( "." ).getFile();
 
-	@BeforeClass
-	public static void startServer() throws Exception {
-		JAXRSServerFactoryBean sf = new JAXRSServerFactoryBean();
-		sf.setResourceClasses(ConverterResourceImpl.class);
-		sf.setProvider(new BinaryFileMessageBodyWriter());
-		sf.setAddress(ROOT_ADDRESS);
-		Server server = sf.create();
-		System.out.println(server.getEndpoint());
-	}
+    @BeforeClass
+    public static void startServer()
+        throws Exception
+    {
+        JAXRSServerFactoryBean sf = new JAXRSServerFactoryBean();
+        sf.setResourceClasses( ConverterResourceImpl.class );
+        sf.setProvider( new BinaryFileMessageBodyWriter() );
+        sf.setAddress( ROOT_ADDRESS );
+        Server server = sf.create();
+        System.out.println( server.getEndpoint() );
+    }
 
-	@Test
-	public void hello() throws InterruptedException {
-		ConverterResource converterService = JAXRSClientFactory.create(
-				BASE_ADDRESS, ConverterResource.class);
-		try {
-			String name = converterService.getText();
+    @Test
+    public void hello()
+        throws InterruptedException
+    {
+        ConverterResource converterService = JAXRSClientFactory.create( BASE_ADDRESS, ConverterResource.class );
+        try
+        {
+            String name = converterService.getText();
 
-			Assert.assertEquals("Hello 0", name);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+            Assert.assertEquals( "Hello 0", name );
+        }
+        catch ( Exception e )
+        {
+            e.printStackTrace();
+        }
 
-	}
+    }
 
+    @Test
+    public void convertPDF_OLD()
+        throws Exception
+    {
+        List<Object> providers = new ArrayList<Object>();
+        providers.add( new BinaryFileMessageBodyReader() );
 
-	@Test
-	public void convertPDF() throws Exception {
-		List<Object> providers = new ArrayList<Object>();
-		providers.add(new BinaryFileMessageBodyReader());
+        ConverterResource converterService =
+            JAXRSClientFactory.create( BASE_ADDRESS, ConverterResource.class, providers );
 
-		ConverterResource converterService = JAXRSClientFactory.create(
-				BASE_ADDRESS, ConverterResource.class, providers);
+        String fileName = "ODTCV.odt";
 
-		String fileName = "ODTCV.odt";
+        FileInputStream fileInputStream = new FileInputStream( new File( root, fileName ) );
+        Request request = new Request();
+        request.setFilename( fileName );
 
-		FileInputStream fileInputStream = new FileInputStream(new File(root,
-				fileName));
-		Request request = new Request();
-		request.setFilename(fileName);
+        int size = fileInputStream.available();
+        byte[] bytes = new byte[size];
+        fileInputStream.read( bytes );
+        fileInputStream.close();
+        request.setContent( bytes );
 
-		int size = fileInputStream.available();
-		byte[] bytes = new byte[size];
-		fileInputStream.read(bytes);
-		fileInputStream.close();
-		request.setContent(bytes);
+        BinaryFile response = converterService.convertPDF( request );
+        Assert.assertEquals( fileName + ".pdf", response.getFileName() );
 
-		BinaryFile response = converterService.convertPDF(request);
-		Assert.assertEquals(fileName + ".pdf", response.getFileName());
+        FileOutputStream out = new FileOutputStream( new File( root, response.getFileName() ) );
 
-		FileOutputStream out = new FileOutputStream(new File(root,
-				response.getFileName()));
+        IOUtils.copyLarge( response.getContent(), out );
+        out.close();
+        System.out.println( size );
+    }
 
-		IOUtils.copyLarge(response.getContent(), out);
-		out.close();
-		System.out.println(size);
-	}
+//    @Test
+    public void convertPDF()
+        throws Exception
+    {
+
+        List<Object> providers = new ArrayList<Object>();
+        providers.add( new BinaryFileMessageBodyReader() );
+        ConverterResource converterService = JAXRSClientFactory.create( BASE_ADDRESS, ConverterResource.class, providers );
+
+        String fileName = "ODTCV.odt";
+
+        FileInputStream fileInputStream = new FileInputStream( new File( root, fileName ) );
+        Request request = new Request();
+        request.setFilename( fileName );
+
+        int size = fileInputStream.available();
+        byte[] bytes = new byte[size];
+        fileInputStream.read( bytes );
+        fileInputStream.close();
+        request.setContent( bytes );
+
+        javax.activation.DataSource dataSource = new ByteDataSource( bytes);
+        Response response = converterService.convert( "PDF", dataSource, "" );
+        InputStream inputStream = (InputStream) response.getEntity();
+        String responseFileName = null;
+        Assert.assertEquals( fileName + ".pdf", responseFileName );
+
+        FileOutputStream out = new FileOutputStream( new File( root, responseFileName ) );
+        IOUtils.copyLarge( inputStream, out );
+        out.close();
+        System.out.println( size );
+    }
 
 }
