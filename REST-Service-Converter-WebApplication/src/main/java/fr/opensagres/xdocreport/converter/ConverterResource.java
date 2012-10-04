eaf8b82cc5f7374f916d36561f641d8fda48f026
angelo.zerr@gmail.com <angelo.zerr@gmail.com>
2012-10-04 16:38:21 +0200

    Use new method convert which works with stream and not with ByteArrayOutputStream (hope Pascal will be OK).

--- REST-Service-Converter-WebApplication/src/main/java/fr/opensagres/xdocreport/converter/ConverterResource.java
+++ REST-Service-Converter-WebApplication/src/main/java/fr/opensagres/xdocreport/converter/ConverterResource.java
@@ -7,29 +7,39 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.apache.cxf.jaxrs.ext.multipart.Multipart;
 
-public interface ConverterResource {
+public interface ConverterResource
+{
 
-
-	@GET
-	@Path("hello")
-	@Produces("text/plain")
-	String getText();
-
-    @POST
-    @Path("convertPDF")
-    @Consumes(MediaType.WILDCARD)
-    @Produces(MediaType.WILDCARD)
-    BinaryFile convertPDF(Request request);
-
-
+    @GET
+    @Path( "hello" )
+    @Produces( "text/plain" )
+    String getText();
 
     @POST
-    @Consumes(MediaType.WILDCARD )
-    @Produces(MediaType.WILDCARD )
-    @Path("/submitForm")
-    BinaryFile submitForm(@Multipart("outputFormat") String outputFormat, @Multipart("datafile") DataSource content,@Multipart("operation") String operation);
+    @Path( "convertPDF" )
+    @Consumes( MediaType.WILDCARD )
+    @Produces( MediaType.WILDCARD )
+    BinaryFile convertPDF( Request request );
 
+    @POST
+    @Consumes( MediaType.WILDCARD )
+    @Produces( MediaType.WILDCARD )
+    @Path( "/submitForm" )
+    BinaryFile submitForm( @Multipart( "outputFormat" )
+    String outputFormat, @Multipart( "datafile" )
+    DataSource content, @Multipart( "operation" )
+    String operation );
+
+    @POST
+    @Consumes( MediaType.WILDCARD )
+    @Produces( MediaType.WILDCARD )
+    @Path( "/convert" )
+    Response convert( @Multipart( "outputFormat" )
+    String outputFormat, @Multipart( "datafile" )
+    DataSource content, @Multipart( "operation" )
+    String operation );
 }
--- REST-Service-Converter-WebApplication/src/main/java/fr/opensagres/xdocreport/converter/internal/ConverterResourceImpl.java
+++ REST-Service-Converter-WebApplication/src/main/java/fr/opensagres/xdocreport/converter/internal/ConverterResourceImpl.java
@@ -4,9 +4,15 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.activation.DataSource;
 import javax.ws.rs.Path;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.StreamingOutput;
 
 import fr.opensagres.xdocreport.converter.BinaryFile;
 import fr.opensagres.xdocreport.converter.ConverterRegistry;
@@ -17,86 +23,187 @@ import fr.opensagres.xdocreport.converter.Options;
 import fr.opensagres.xdocreport.converter.Request;
 import fr.opensagres.xdocreport.converter.XDocConverterException;
 import fr.opensagres.xdocreport.core.document.DocumentKind;
+import fr.opensagres.xdocreport.core.io.IOUtils;
+import fr.opensagres.xdocreport.core.utils.HttpHeaderUtils;
 
 /**
  * REST Web Service
- *
+ * 
  * @author pleclercq
  */
-@Path("generic")
-public class ConverterResourceImpl implements ConverterResource {
+@Path( "/" )
+public class ConverterResourceImpl
+    implements ConverterResource
+{
 
-	private static int i = 0;
+    private static final String DOWNLOAD_OPERATION = "download";
 
-	public String getText() {
-		return "Hello " + i++;
-	}
+    private static int i = 0;
 
-	public BinaryFile convertPDF(Request request) {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		// 1) Create options ODT 2 PDF to select well converter form the
-		// registry
-		Options options = Options.getFrom(DocumentKind.ODT).to(
-				ConverterTypeTo.PDF);
+    public String getText()
+    {
+        return "Hello " + i++;
+    }
 
-		// 2) Get the converter from the registry
-		IConverter converter = ConverterRegistry.getRegistry().getConverter(
-				options);
+    public BinaryFile convertPDF( Request request )
+    {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        // 1) Create options ODT 2 PDF to select well converter form the
+        // registry
+        Options options = Options.getFrom( DocumentKind.ODT ).to( ConverterTypeTo.PDF );
 
-		// 3) Convert ODT 2 PDF
+        // 2) Get the converter from the registry
+        IConverter converter = ConverterRegistry.getRegistry().getConverter( options );
 
-		try {
+        // 3) Convert ODT 2 PDF
 
-			converter.convert(new ByteArrayInputStream(request.getContent()),
-					out, options);
-		} catch (XDocConverterException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        try
+        {
 
-		BinaryFile response = new BinaryFile();
+            converter.convert( new ByteArrayInputStream( request.getContent() ), out, options );
+        }
+        catch ( XDocConverterException e )
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-		response.setContent(new ByteArrayInputStream(out.toByteArray()));
-		response.setFileName(request.getFilename() + ".pdf");
+        BinaryFile response = new BinaryFile();
 
-		return response;
-	}
+        response.setContent( new ByteArrayInputStream( out.toByteArray() ) );
+        response.setFileName( request.getFilename() + ".pdf" );
 
-	public BinaryFile submitForm(String outputFormat, DataSource content,String operation) {
+        return response;
+    }
 
-		System.out.println(operation);
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		String filename = content.getName();
-		String mimetype = content.getContentType();
+    public BinaryFile submitForm( String outputFormat, DataSource content, String operation )
+    {
 
-		ConverterTypeTo to = ConverterTypeTo.valueOf(outputFormat);
-		// 1) Create options ODT 2 PDF to select well converter form the
-		// registry
-		// DocumentKind.ODT
-		Options options = Options.getFrom(DocumentKind.fromMimeType(mimetype))
-				.to(to);
-		// 2) Get the converter from the registry
-		IConverter converter = ConverterRegistry.getRegistry().getConverter(
-				options);
+        System.out.println( operation );
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        String filename = content.getName();
+        String mimetype = content.getContentType();
 
-		// 3) Convert ODT 2 PDF
-		try {
-			converter.convert(content.getInputStream(),out, options);
+        ConverterTypeTo to = ConverterTypeTo.valueOf( outputFormat );
+        // 1) Create options ODT 2 PDF to select well converter form the
+        // registry
+        // DocumentKind.ODT
+        Options options = Options.getFrom( DocumentKind.fromMimeType( mimetype ) ).to( to );
+        // 2) Get the converter from the registry
+        IConverter converter = ConverterRegistry.getRegistry().getConverter( options );
 
-		} catch (XDocConverterException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        // 3) Convert ODT 2 PDF
+        try
+        {
+            converter.convert( content.getInputStream(), out, options );
 
-		BinaryFile output = new BinaryFile();
-		if("download".equals(operation)){
-			output.setFileName(filename.replace('.', '_') + "."+ to.getExtension());
-		}
+        }
+        catch ( XDocConverterException e )
+        {
+            e.printStackTrace();
+        }
+        catch ( IOException e )
+        {
+            e.printStackTrace();
+        }
 
-		InputStream result = new ByteArrayInputStream(out.toByteArray());
-		output.setContent(result);
-		output.setMimeType(to.getMimeType());
-		return output;
-	}
+        BinaryFile output = new BinaryFile();
+        if ( isDownload( operation ) )
+        {
+            output.setFileName( getOutputFileName( filename, to ) );
+        }
+
+        InputStream result = new ByteArrayInputStream( out.toByteArray() );
+        output.setContent( result );
+        output.setMimeType( to.getMimeType() );
+        return output;
+    }
+
+    public Response convert( String outputFormat, final DataSource content, String operation )
+    {
+        try
+        {
+            // 1) Get the converter type to use
+            ConverterTypeTo to = ConverterTypeTo.valueOf( outputFormat );
+            if ( to == null )
+            {
+                throw new XDocConverterException( "Converter service cannot support the output format=" + outputFormat );
+            }
+            // 2) retrieve the document kind from the input mimeType
+            String mimeType = content.getContentType();
+            DocumentKind documentKind = DocumentKind.fromMimeType( mimeType );
+            if ( documentKind == null )
+            {
+                throw new XDocConverterException( "Converter service cannot support mime-type=" + mimeType );
+            }
+            // 3) Get the converter from the registry
+            final Options options = Options.getFrom( documentKind ).to( to );
+            final IConverter converter = ConverterRegistry.getRegistry().getConverter( options );
+
+            // 4) Create an instance of JAX-RS StreamingOutput to convert the inputstream and set the result in the
+            // response stream.
+            StreamingOutput output = new StreamingOutput()
+            {
+                public void write( OutputStream out )
+                    throws IOException, WebApplicationException
+                {
+                    try
+                    {
+                        converter.convert( content.getInputStream(), out, options );
+                    }
+                    catch ( XDocConverterException e )
+                    {
+                        throw new WebApplicationException( e );
+                    }
+                    finally
+                    {
+                        IOUtils.closeQuietly( out );
+                    }
+
+                }
+            };
+            // 5) Create the JAX-RS response builder.
+            ResponseBuilder responseBuilder = Response.ok( output, MediaType.valueOf( to.getMimeType() ) );
+            if ( isDownload( operation ) )
+            {
+                // The converted document must be downloaded, add teh well content-disposition header.
+                String fileName = content.getName();
+                responseBuilder.header( HttpHeaderUtils.CONTENT_DISPOSITION_HEADER,
+                                        HttpHeaderUtils.getAttachmentFileName( getOutputFileName( fileName, to ) ) );
+            }
+            return responseBuilder.build();
+
+        }
+        catch ( XDocConverterException e )
+        {
+            throw new WebApplicationException( e );
+
+        }
+    }
+
+    /**
+     * Returns true if operation is download and false otherwise.
+     * 
+     * @param operation
+     * @return
+     */
+    private boolean isDownload( String operation )
+    {
+        return DOWNLOAD_OPERATION.equals( operation );
+    }
+
+    /**
+     * Returns the output file name.
+     * 
+     * @param filename
+     * @param to
+     * @return
+     */
+    protected String getOutputFileName( String filename, ConverterTypeTo to )
+    {
+        StringBuilder name = new StringBuilder( filename.replace( '.', '_' ) );
+        name.append( '.' );
+        name.append( to.getExtension() );
+        return name.toString();
+    }
 }
