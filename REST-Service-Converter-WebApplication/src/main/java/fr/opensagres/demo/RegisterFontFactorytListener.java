54e17b150c9de7b1b33c620af799d20ba0c0f9d7
pascalleclercq <pascal.leclercq@gmail.com>
2013-09-18 23:48:58 +0200

    Idea : put fonts under private coudbees folder

--- REST-Service-Converter-WebApplication/src/main/java/fr/opensagres/demo/RegisterFontFactorytListener.java
+++ REST-Service-Converter-WebApplication/src/main/java/fr/opensagres/demo/RegisterFontFactorytListener.java
@@ -1,26 +1,40 @@
 package fr.opensagres.demo;
 
+import java.io.File;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.lowagie.text.FontFactory;
 
 public class RegisterFontFactorytListener implements ServletContextListener {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(RegisterFontFactorytListener.class);
+	
 	public void contextDestroyed(ServletContextEvent event) {}
 
 	public void contextInitialized(ServletContextEvent event) {
-		System.out.println(FontFactory.getRegisteredFonts());
-		System.out.println("------------------------------");
-		System.out.println(FontFactory.getFont("times-roman").hashCode());
-		String fontFolder = event.getServletContext().getRealPath("font");
+		LOGGER.info(FontFactory.getRegisteredFonts().toString());
 		
-		FontFactory.register(fontFolder+"/Aller_Rg.ttf","times-roman");
-		FontFactory.register(fontFolder+"/Aller_BdIt.ttf","times-bolditalic");
-		FontFactory.register(fontFolder+"/Aller_Bd.ttf","times-bold");
-		FontFactory.register(fontFolder+"/Aller_It.ttf","times-italic");
-		System.out.println(FontFactory.getFont("times-roman").hashCode());
-		System.out.println(FontFactory.getRegisteredFonts());
+		File aFile = new File("/private/opensagres/fonts");
+		LOGGER.info("Folder exists ? "+ aFile.exists());
+		if(aFile.exists()) {
+			File font = new File(aFile,"calibri.ttf");
+			LOGGER.info("font exists ? "+ font.exists());
+		}
+//		System.out.println("------------------------------");
+//		System.out.println(FontFactory.getFont("times-roman").hashCode());
+//		String fontFolder = event.getServletContext().getRealPath("font");
+//		
+//		FontFactory.register(fontFolder+"/Aller_Rg.ttf","times-roman");
+//		FontFactory.register(fontFolder+"/Aller_BdIt.ttf","times-bolditalic");
+//		FontFactory.register(fontFolder+"/Aller_Bd.ttf","times-bold");
+//		FontFactory.register(fontFolder+"/Aller_It.ttf","times-italic");
+//		System.out.println(FontFactory.getFont("times-roman").hashCode());
+//		System.out.println(FontFactory.getRegisteredFonts());
 
 	}
 
